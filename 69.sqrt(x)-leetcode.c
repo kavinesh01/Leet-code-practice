113d15eeb6c3ef4d27b44810fd4a1f90555ec161
kavinesh01 <kavineshsankar1@gmail.com>
2026-02-19 12:21:13 +0530

    Implement binary search for square root calculation

---
+++ 69.sqrt(x)-leetcode.c
@@ -0,0 +1,20 @@
+int mySqrt(int x) {
+    if (x < 2)
+        return x;
+
+    int left = 1, right = x;
+    int ans = 0;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (mid <= x / mid) {   // mid*mid <= x (safe check)
+            ans = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return ans;
+}
